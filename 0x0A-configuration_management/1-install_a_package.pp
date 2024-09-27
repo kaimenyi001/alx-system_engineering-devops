a9e7ed301e36343dd5b63cc0e2d1f8c4363baa8f
kaimenyi001 <deniskaimenyi1@gmail.com>
2024-09-27 00:25:54 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-# Install Flask version 2.1.0 
+#Install flask
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3'
+}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask']
 }
